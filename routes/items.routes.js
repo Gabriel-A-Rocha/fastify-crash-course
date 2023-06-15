91fe093bab6d535f871607eb7ed1a73246db9755
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2023-06-15 19:36:15 -0400

    add validation schemas

--- routes/items.routes.js
+++ routes/items.routes.js
@@ -1,11 +1,44 @@
 const items = require("../Items");
 
+const ItemSchema = {
+  type: "object",
+  properties: {
+    id: {
+      type: "string",
+    },
+    name: {
+      type: "string",
+    },
+  },
+};
+
+const getItemsOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: ItemSchema,
+      },
+    },
+  },
+};
+
+const getItemOptions = {
+  schema: {
+    response: {
+      200: ItemSchema,
+    },
+  },
+};
+
 function itemsRoutes(fastify, options, done) {
-  fastify.get("/items", (request, reply) => {
+  // get all items
+  fastify.get("/items", getItemsOptions, (request, reply) => {
     reply.send(items);
   });
 
-  fastify.get("/items/:itemID", (request, reply) => {
+  // get single item
+  fastify.get("/items/:itemID", getItemOptions, (request, reply) => {
     const { itemID } = request.params;
     const record = items.find((item) => item.id === itemID);
     reply.send({ ...record });
