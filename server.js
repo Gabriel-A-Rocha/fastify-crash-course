890e6aa652838e4afeb6578b5b4b46eff3d0633c
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2023-06-15 19:17:05 -0400

    create two endpoints in the entry file

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const fastify = require("fastify")({ logger: true });
+const items = require("./Items");
 
 const PORT = 5000;
+
+fastify.get("/items", (request, reply) => {
+  reply.send(items);
+});
+
+fastify.get("/items/:itemID", (request, reply) => {
+  const { itemID } = request.params;
+  const record = items.find((item) => item.id === itemID);
+  reply.send({ ...record });
+});
 
 const start = async () => {
   try {
