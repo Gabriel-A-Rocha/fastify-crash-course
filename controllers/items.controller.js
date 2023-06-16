52997adc93498be3e81c4fa9cfc209949220051b
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2023-06-15 21:59:58 -0400

    add remaining CRUD endpoints

--- controllers/items.controller.js
+++ controllers/items.controller.js
@@ -1,4 +1,6 @@
-const items = require("../Items");
+const { v4: uuidV4 } = require("uuid");
+
+let items = require("../Items");
 
 const getItems = (request, reply) => {
   reply.send(items);
@@ -10,4 +12,33 @@ const getItem = (request, reply) => {
   reply.send({ ...record });
 };
 
-module.exports = { getItem, getItems };
+const addItem = (request, reply) => {
+  const { name } = request.body;
+  const newItem = { id: uuidV4(), name };
+  items.push(newItem);
+  reply.code(201).send(newItem);
+};
+
+const deleteItem = (request, reply) => {
+  const { itemID } = request.params;
+  items = items.filter((item) => item.id !== itemID);
+  reply.code(200).send({ message: `Item ${itemID} deleted successfully` });
+};
+
+const updateItem = (request, reply) => {
+  const { itemID } = request.params;
+  const { name } = request.body;
+  const updatedItem = {
+    id: itemID,
+    name,
+  };
+  items = items.map((item) => {
+    if (item.id === itemID) {
+      return updatedItem;
+    }
+    return item;
+  });
+  reply.code(200).send(updatedItem);
+};
+
+module.exports = { getItem, getItems, addItem, deleteItem, updateItem };
--- routes/items.routes.js
+++ routes/items.routes.js
@@ -1,4 +1,10 @@
-const { getItems, getItem } = require("../controllers/items.controller");
+const {
+  getItems,
+  getItem,
+  addItem,
+  deleteItem,
+  updateItem,
+} = require("../controllers/items.controller");
 
 const ItemSchema = {
   type: "object",
@@ -33,12 +39,64 @@ const getItemOptions = {
   handler: getItem,
 };
 
+const addItemOptions = {
+  schema: {
+    body: {
+      type: "object",
+      required: ["name"],
+      properties: {
+        name: {
+          type: "string",
+        },
+      },
+    },
+    response: {
+      200: ItemSchema,
+    },
+  },
+  handler: addItem,
+};
+
+const deleteItemOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          message: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+  handler: deleteItem,
+};
+
+const updateItemOptions = {
+  schema: {
+    response: {
+      200: ItemSchema,
+    },
+  },
+  handler: updateItem,
+};
+
 function itemsRoutes(fastify, options, done) {
   // get all items
   fastify.get("/items", getItemsOptions);
 
   // get single item
   fastify.get("/items/:itemID", getItemOptions);
+
+  // add item
+  fastify.post("/items", addItemOptions);
+
+  // delete item
+  fastify.delete("/items/:itemID", deleteItemOptions);
+
+  // update item
+  fastify.patch("/items/:itemID", updateItemOptions);
 
   done();
 }
